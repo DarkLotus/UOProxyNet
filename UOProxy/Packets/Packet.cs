e3a7c753e10b077220c73c1bd4e93c0234196f3a
james <kklolk@gmail.com>
2012-02-25 13:36:04 +1100

    Inital Commit

---
+++ UOProxy/Packets/Packet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace UOProxy.Packets
+{
+    public class Packet
+    {
+        public UOStream Data;
+        public byte OpCode;
+        public Packet(byte[] data)
+        {
+            this.Data = new UOStream(data);
+            this.OpCode = this.Data.ReadBit();
+        }
+        public Packet()
+        {
+            Data = new UOStream();
+        }
+        public Packet(byte OpCode)
+        {
+            Data = new UOStream();
+            Data.WriteByte(OpCode);
+        }
+    }
+}
